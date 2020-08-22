a182cf7580ac39b6ab3176a32aa3d5bddcae3cd0
sunchara <sunchara@gmail.com>
2020-08-22 22:46:55 +0300

    version 1.2

--- gvc.cpp
+++ gvc.cpp
@@ -7,15 +7,17 @@
 #include <math.h>
 #include <algorithm>
 #include <string>
-static int X,Y;
-static bool moved = false;
-static int radius;
-static int range;
+static int X,Y;// pos of mouse
+static bool moved = false;// is moved
+static int radius; //radius of the blured circle in pixels
+static int range; //kernel is (2*range+1,2*range+1) matrix
 
 inline bool isInRadius(int x,int y)
 {
   return (x-X)*(x-X)+(y-Y)*(y-Y)<=radius*radius;
 }
+
+//mouse move event handler
 void mouseEventcalback(int event, int y, int x, int flags, void* userdata)
 {
     if(event == cv::EVENT_MOUSEMOVE)
@@ -23,21 +25,25 @@ void mouseEventcalback(int event, int y, int x, int flags, void* userdata)
         X=x;
         Y=y;
         moved=true;
-        //std::cerr<<"Mousemoved  "<<x<<' '<<y<<std::endl;
     }
 }
-void drawBlur(const cv::Mat & frame)//, int x, int y)
+
+
+void drawBlur(const cv::Mat & frame)
 {
+
   int n=0;
   double sum[3]={0,0,0};
-  cv::Mat  blur=frame.clone();
+  cv::Mat  blur=frame.clone(); // copy the frame we want to be blured
 
   for(int r=0;r<frame.rows;++r)
     for(int c=0;c<frame.cols;++c){
       if(isInRadius(r,c))
       {
-        n=0;
-        sum[0]=sum[1]=sum[2]=0;
+        n=0; //number of components to sum
+        sum[0]=sum[1]=sum[2]=0;// 3 chanels to summ
+
+        //filtering
         for(int i=std::max(r-range, 0);i<=std::min(r+range,frame.rows);++i)
           for(int j=std::max(c-range, 0);j<=std::min(c+range,frame.cols);++j)
           {
@@ -46,29 +52,38 @@ void drawBlur(const cv::Mat & frame)//, int x, int y)
             sum[2]+=frame.at<cv::Vec3b>(i,j)[2];
             ++n;
           }
-          
-          blur.at<cv::Vec3b>(r,c)[0] = sum[0]/(double)n;
-          blur.at<cv::Vec3b>(r,c)[1] = sum[1]/(double)n;
-          blur.at<cv::Vec3b>(r,c)[2] = sum[2]/(double)n;
+
+        //writing the result in Mat blur
+        blur.at<cv::Vec3b>(r,c)[0] = sum[0]/(double)n;
+        blur.at<cv::Vec3b>(r,c)[1] = sum[1]/(double)n;
+        blur.at<cv::Vec3b>(r,c)[2] = sum[2]/(double)n;
 
       }
     }
-    blur.copyTo(frame);
+    blur.copyTo(frame);// cope the result back
 }
 int main(int argc, char *argv[])
 {
-
+    cv::String src;
+    //read ini file
     INIReader inireader("gvc.ini");
     radius = inireader.GetInteger("config", "radius", 30);
     range = inireader.GetInteger("config", "range", 2);
+    std::string _src = inireader.GetString("config", "source_url","");
 
+    // get source url from shel or ini file
     if(argc < 2)
     {
-      std::cerr<<"no arguments passed"<<std::endl;
-      return -1;
+      if(_src==""){
+        std::cerr<<"no arguments passed"<<std::endl;
+        return -1;
+      }
+      else
+        src=_src;
     }
+    else
+      src = argv[1];
 
-    cv::String src(argv[1]);// = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
     std::cerr<<"source is "<<src<<std::endl;
     cv::VideoCapture v(src);
 
@@ -93,9 +108,13 @@ int main(int argc, char *argv[])
             drawBlur(frame);
           }
         imshow("vid",frame);
+
+        //'ESC' to exit
         if(cv::waitKey(1) == 27)
             break;
     }
+
+
     v.release();
     cv::destroyAllWindows();
     return 0;
